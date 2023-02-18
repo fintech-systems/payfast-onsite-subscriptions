8faa5d11f597e1894370f0a11027c1c43214c255
Eugene van der Merwe <eugenevdm@gmail.com>
2023-02-18 08:44:43 +0200

    remove app_key

--- tests/Feature/WebhooksTest.php
+++ tests/Feature/WebhooksTest.php
@@ -2,15 +2,6 @@
 
 namespace Tests\Feature;
 
-use FintechSystems\PayFast\Cashier;
-use FintechSystems\PayFast\Events\PaymentSucceeded;
-use FintechSystems\PayFast\Events\SubscriptionCancelled;
-use FintechSystems\PayFast\Events\SubscriptionCreated;
-use FintechSystems\PayFast\Events\SubscriptionPaymentSucceeded;
-use FintechSystems\PayFast\Events\SubscriptionUpdated;
-use FintechSystems\PayFast\Subscription;
-use Illuminate\Support\Facades\Http;
-
 class WebhooksTest extends FeatureTestCase
 {
     public function test_gracefully_handle_webhook_without_alert_name()
